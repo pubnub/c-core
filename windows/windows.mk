2fab59b5f56e3b00998cf71f92cc26d3d7595898
unknown <vveljko@github.com>
2015-08-04 13:13:19 +0200

    Added the "subscribe & publish w/callback" example.

--- windows/windows.mk
+++ windows/windows.mk
@@ -3,13 +3,16 @@ SOURCEFILES = ..\core\pubnub_coreapi.c ..\core\pubnub_ccore.c ..\core\pubnub_net
 CFLAGS = -Zi -D VERBOSE_DEBUG -I ..\core -I . -I fntest -I ../core/fntest -I ..\core\c99 /W3
 # -Zi enables debugging, remove to get a smaller .exe and no .pdb
 
-all: pubnub_sync_sample.exe pubnub_callback_sample.exe pubnub_fntest.exe
+all: pubnub_sync_sample.exe subscribe_publish_callback_sample.exe pubnub_callback_sample.exe pubnub_fntest.exe
 
 pubnub_sync_sample.exe: ..\core\samples\pubnub_sync_sample.c $(SOURCEFILES) ..\core\pubnub_ntf_sync.c
 	cl $(CFLAGS) ..\core\samples\pubnub_sync_sample.c $(SOURCEFILES) ..\core\pubnub_ntf_sync.c ws2_32.lib rpcrt4.lib
 
 pubnub_callback_sample.exe: ..\core\samples\pubnub_callback_sample.c $(SOURCEFILES) pubnub_ntf_callback_windows.c
 	cl $(CFLAGS) -DPUBNUB_CALLBACK_API ..\core\samples\pubnub_callback_sample.c  $(SOURCEFILES) pubnub_ntf_callback_windows.c ws2_32.lib rpcrt4.lib
+
+subscribe_publish_callback_sample.exe: ..\core\samples\subscribe_publish_callback_sample.c $(SOURCEFILES) pubnub_ntf_callback_windows.c
+	cl $(CFLAGS) -DPUBNUB_CALLBACK_API ..\core\samples\subscribe_publish_callback_sample.c  $(SOURCEFILES) pubnub_ntf_callback_windows.c ws2_32.lib rpcrt4.lib
 
 pubnub_fntest.exe: ..\core\fntest\pubnub_fntest.c ..\core\fntest\pubnub_fntest_basic.c ..\core\fntest\pubnub_fntest_medium.c  fntest\pubnub_fntest_windows.c fntest\pubnub_fntest_runner.c $(SOURCEFILES)  ..\core\pubnub_ntf_sync.c
   cl $(CFLAGS) ..\core\fntest\pubnub_fntest.c ..\core\fntest\pubnub_fntest_basic.c ..\core\fntest\pubnub_fntest_medium.c fntest\pubnub_fntest_windows.c fntest\pubnub_fntest_runner.c $(SOURCEFILES)  ..\core\pubnub_ntf_sync.c ws2_32.lib rpcrt4.lib user32.lib
