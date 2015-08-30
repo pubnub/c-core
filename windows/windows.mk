539359f0e11a35009de4f606236ecf63df2ddfd0
unknown <vveljko@github.com>
2015-08-30 14:19:32 +0200

    Added the /MP switch to enable faster builds on multi-core hosts.

--- windows/windows.mk
+++ windows/windows.mk
@@ -1,7 +1,8 @@
 SOURCEFILES = ..\core\pubnub_coreapi.c ..\core\pubnub_ccore.c ..\core\pubnub_netcore.c  ..\lib\sockets\pbpal_sockets.c ..\lib/sockets\pbpal_resolv_and_connect_sockets.c ..\core\pubnub_alloc_std.c ..\core\pubnub_assert_std.c ..\core\pubnub_generate_uuid.c ..\core\pubnub_blocking_io.c ..\core\pubnub_json_parse.c ..\core\pubnub_helper.c pubnub_version_windows.c  pubnub_generate_uuid_windows.c pbpal_windows_blocking_io.c ..\core\c99\snprintf.c
 
-CFLAGS = -Zi -D VERBOSE_DEBUG -I ..\core -I . -I fntest -I ../core/fntest -I ..\core\c99 /W3
-# -Zi enables debugging, remove to get a smaller .exe and no .pdb
+CFLAGS = /Zi /MP /D VERBOSE_DEBUG /I ..\core /I . /I fntest /I ../core/fntest /I ..\core\c99 /W3
+# /Zi enables debugging, remove to get a smaller .exe and no .pdb
+# /MP uses one compiler (`cl`) process for each input file, enabling faster build
 
 all: pubnub_sync_sample.exe subscribe_publish_callback_sample.exe pubnub_callback_sample.exe pubnub_fntest.exe
 
