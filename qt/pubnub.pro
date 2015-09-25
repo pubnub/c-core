36eab7b04b1f5f13d9d334b423ff539501c094ca
Vladimir Veljkovic <vveljko@github.com>
2015-09-25 17:04:33 +0200

    Fix Qt on Windows / MSVC
    Properly configure the Qt project and fix MSVC issues (including a nasty
    alignment one)

--- qt/pubnub.pro
+++ qt/pubnub.pro
@@ -4,8 +4,10 @@ mac:CONFIG -= app_bundle
 win32:CONFIG += console
 HEADERS += pubnub_qt.h pubnub_qt_sample.h
 SOURCES += pubnub_qt.cpp pubnub_qt_sample.cpp ../core/pubnub_ccore.c ../core/pubnub_assert_std.c ../core/pubnub_json_parse.c ../core/pubnub_helper.c
+win32:SOURCES += ../core/c99/snprintf.c
 
 INCLUDEPATH += ../core
+win32:INCLUDEPATH += ../core/c99
 DEPENDPATH += ../core
 
 # install
--- qt/pubnub_internal.h
+++ qt/pubnub_internal.h
@@ -4,14 +4,18 @@
 
 
 
-
 typedef int pb_socket_t;
 
 /** The Pubnub Qt (fake) context */
 struct pubnub_pal {
+    /* Some compilers insist on having at least one member
+     * in a struct, so, let's make them happy, but this is
+     * never used in Pubnuf for Qt.
+     */
+    pb_socket_t fake;
 };
 
-
+/** The way we use Qt, blocking I/O is not applicable */
 #define PUBNUB_BLOCKING_IO_SETTABLE 0
 
 
