c19e96266b9925145dc981d646715267a13faf17
Vladimir Veljkovic <vl.veljkovic@yahoo.com>
2016-01-20 20:42:38 +0100

    Threadsafe fixed and tested on FreeRTOS(+TCP)

--- freertos/pbpal_mutex.h
+++ freertos/pbpal_mutex.h
@@ -6,6 +6,7 @@
     PAL for mutex(es) on FreeRTOS
  */
 
+#include "semphr.h"
 
 typedef SemaphoreHandle_t pbpal_mutex_t;
 
@@ -14,8 +15,15 @@ typedef SemaphoreHandle_t pbpal_mutex_t;
 #define pbpal_mutex_unlock(m) xSemaphoreGive(m)
 #define pbpal_mutex_destroy(m) vSemaphoreDelete(m)
 #define pbpal_mutex_decl_and_init(m) SemaphoreHandle_t m = xSemaphoreCreateMutex()
-#define pbpal_mutex_static_decl_and_init(m) static SemaphoreHandle_t m = xSemaphoreCreateMutex()
-#define pbpal_mutex_init_static(m)
+#define pbpal_mutex_static_decl_and_init(m) static SemaphoreHandle_t m; static int m_init_##m
+#define pbpal_mutex_init_static(m) do { \
+    taskENTER_CRITICAL(); \
+    if (0 == m_init_##m) { \
+        m = xSemaphoreCreateMutex(); \
+        m_init_##m = 1; \
+    } \
+    taskEXIT_CRITICAL(); \
+} while(0)
 
 
 #endif /*!defined INC_PBPAL_MUTEX*/
