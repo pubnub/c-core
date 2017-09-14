80eeb513f8438bc59be2e74c4260bd6017723abd
Vladimir Veljkovic <vl.veljkovic@yahoo.com>
2017-09-14 20:45:09 +0200

    Bump version to 2.2.7

--- microchip_harmony/pubnub_version_harmony.c
+++ microchip_harmony/pubnub_version_harmony.c
@@ -2,7 +2,7 @@
 #include "pubnub_version.h"
 
 #define PUBNUB_SDK_NAME "Microchip.Harmony"
-#define PUBNUB_SDK_VERSION "2.2.6"
+#define PUBNUB_SDK_VERSION "2.2.7"
 
 
 char const *pubnub_sdk_name(void)
