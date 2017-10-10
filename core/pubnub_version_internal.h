95c629782a429452c0cef4151a499ea1ce8786ca
Vladimir Veljkovic <vl.veljkovic@yahoo.com>
2017-10-10 21:06:19 +0200

    Add internal/common version (number) header file.

---
+++ core/pubnub_version_internal.h
@@ -0,0 +1,10 @@
+/* -*- c-file-style:"stroustrup"; indent-tabs-mode: nil -*- */
+#if !defined INC_PUBNUB_VERSION_INTERNAL
+#define INC_PUBNUB_VERSION_INTERNAL
+
+
+#define PUBNUB_SDK_VERSION "2.2.11"
+
+
+
+#endif /* !defined INC_PUBNUB_VERSION_INTERNAL */
