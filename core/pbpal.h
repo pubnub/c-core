pubnub/c-core

commit 03dfd2cbf03cd998f8c4c361b5fdca7e01b83536

Improve error handling.
* recv() can actually "raise" EAGAIN on a blocking socket (if a timeout is
  set on said socket - and the timeout expires)
* POSIX allows EAGAIN to be used anywhere EWOULDBLOCK is used (and they need not
  have equal values)
* We now handle errors during reading of the body of the HTTP response in the
  same way as during reading of the HTTP header lines

core/pbpal.h
@@ -105,8 +105,9 @@ int pbpal_start_read_line(pubnub_t *pb);
 
 /** Returns the status of reading a line. Line reading was
     started with pbpal_start_read_line().
-    @return 0: line was read, +1: line reading still in progress
-    -1: line reading failed
+    @retval PNR_IN_PROGRESS reading a line not over/done yet
+    @retval PNR_OK Reading a line done
+    @retval otherwise The error that happened during the reading
 */
 enum pubnub_res pbpal_line_read_status(pubnub_t *pb);
 
@@ -119,7 +120,7 @@ int pbpal_read_len(pubnub_t *pb);
     established TCP connection. Only one reading can take place at any
     given time.
 
-    To check if reading is complete, call pbpal_read_over().
+    To check if reading is complete, call pbpal_read_status().
 
     @param pb The Pubnub context of an established TCP connection
     @param n Number of octets (bytes) to read
@@ -127,10 +128,13 @@ int pbpal_read_len(pubnub_t *pb);
 */
 int pbpal_start_read(pubnub_t *pb, size_t n);
 
-/** Returns if reading is done (has the requested number of octets
-    (bytes) been read).
+/** Returns the status of reading a chunk of data. In general, it's
+    used to receive the body (or chunk of it) of the HTTP response.
+    @retval PNR_IN_PROGRESS reading a chunk not over/done yet
+    @retval PNR_OK Reading a chunk done
+    @retval otherwise The error that happened during the reading
 */
-bool pbpal_read_over(pubnub_t *pb);
+enum pubnub_res pbpal_read_status(pubnub_t *pb);
 
 /** Returns whether for the given Pubnub context the TCP
     connection has closed.

commit 2b8b5f0f44da27d5854ba0a5f8dd1a26a1225618

Fix receiving data longer thana http_buf at a time
If we had received data from Pubnub server that is longer than the
size of our HTTP ("scratchpad") buffer, we would mishandle that. It
wouldn't show often, as default size of the buffer on Windows and
POSIX was 32000 octets, which is longer than most HTTP replies.

This was actually a regression introduced in commit
03dfd2cbf03cd998f8c4c361b5fdca7e01b83536.

This prompted a "small scale refactoring" of the buffer handling,
which is now a little simpler, more resilent, uses less memory and
fixes some other corner-cases which probably couldn't happen "as-is",
but, just like this regression, could have "awaken" with some other
"innocent looking" change.

.pubnub.yml
@@ -2,6 +2,10 @@ name: c-core
 version: 2.2.12
 scm: github.com/pubnub/c-core
 changelog:
+  - version: v2.2.13
+    changes:
+      - type: bug
+        text: Fix receiving data longer than http_buff at a time
   - version: v2.2.12
     changes:
       - type: bug

core/pbpal.h
@@ -55,7 +55,10 @@ enum pbpal_resolv_n_connect_result {
 */
 enum pbpal_resolv_n_connect_result pbpal_resolv_and_connect(pubnub_t *pb);
 
-
+/** This checks for the status of a TCP (HTTP) connection
+    establishing, which includes the DNS resolution. It's used if this
+    wasn't done synchronously during pbpal_resolve_and_connect().
+*/
 enum pbpal_resolv_n_connect_result pbpal_check_resolv_and_connect(pubnub_t *pb);
 
 /** Sends data over an established connection (with the Pubnub server).
@@ -74,10 +77,12 @@ int pbpal_send(pubnub_t *pb, void const *data, size_t n);
 
 /** Helper macro for optimisation of sending of literal strings.
     We know their length, we don't have to call strlen().
+
+    @note There's no way to detect if @p litstr is really a
+    literal string, so, handle with care.
  */
-#define pbpal_send_literal_str(pb, litstr) {          \
-        uint8_t s_[] = litstr;                        \
-        pbpal_send((pb), s_, sizeof s_ - 1); }
+#define pbpal_send_literal_str(pb, litstr) \
+        pbpal_send((pb), litstr, sizeof litstr - 1)
 
 /** The effect of this is the same as:
 
@@ -96,17 +101,33 @@ int pbpal_send_str(pubnub_t *pb, char const *s);
 int pbpal_send_status(pubnub_t *pb);
 
 /** Starts reading a line from the TCP connection. In other words,
-    reading until it finds a newline character.
+    reading until it finds a newline character. In general, it's used
+    to read HTTP response header lines.
 
-    @param pb The Pubnub context of the connection 
+    @precondition Previous read (or write) on the context was finished
+
+    @param pb The Pubnub context of the connection
+
     @return 0: newline read, else: not yet, try again later
 */
 int pbpal_start_read_line(pubnub_t *pb);
 
 /** Returns the status of reading a line. Line reading was
     started with pbpal_start_read_line().
+
+    @param pb The Pubnub context to get line reading status for
+
     @retval PNR_IN_PROGRESS reading a line not over/done yet
-    @retval PNR_OK Reading a line done
+
+    @retval PNR_TX_BUFFER_TOO_SMALL Well, it's not the _TX_ buffer,
+    but, it's the same buffer and we don't want to add a new `PNR`
+    just for internal purposes. Client may examine the contents of the
+    buffer for some further processing and can continue reading (with
+    the current buffer contents dropped) by calling
+    pbpal_start_read_line().
+
+    @retval PNR_OK Reading a line done (newline char found)
+
     @retval otherwise The error that happened during the reading
 */
 enum pubnub_res pbpal_line_read_status(pubnub_t *pb);
@@ -122,6 +143,8 @@ int pbpal_read_len(pubnub_t *pb);
 
     To check if reading is complete, call pbpal_read_status().
 
+    @precondition Previous read (or write) on the context was finished
+
     @param pb The Pubnub context of an established TCP connection
     @param n Number of octets (bytes) to read
     @return 0: OK (started), -1: error (reading already started)
@@ -130,8 +153,21 @@ int pbpal_start_read(pubnub_t *pb, size_t n);
 
 /** Returns the status of reading a chunk of data. In general, it's
     used to receive the body (or chunk of it) of the HTTP response.
-    @retval PNR_IN_PROGRESS reading a chunk not over/done yet
-    @retval PNR_OK Reading a chunk done
+
+    @param pb The Pubnub context for which to get the current read
+    (chunk) status
+
+    @retval PNR_IN_PROGRESS reading a chunk not over/done yet (AKA
+    "call me later")
+
+    @retval PNR_OK Reading a chunk done - it _doesn't_ mean that
+    all of the `n` octets from the pbpal_start_read() were read.
+    It just means that we have read _some_ data and "client" needs
+    to use it somehow (copy, probably). The amout of octets 
+    actually read client can get by calling pbpal_read_len(). 
+    If not all data was read, client should call pbpal_start_read()
+    with `n = old n - pbpal_read_len()`.
+    
     @retval otherwise The error that happened during the reading
 */
 enum pubnub_res pbpal_read_status(pubnub_t *pb);

core/pubnub_assert.h
@@ -58,7 +58,7 @@
 
 /** The common ASSERT implementation */
 #define PUBNUB_ASSERT_IMPL(X) do {                                      \
-        PUBNUB_ANALYSIS_ASSUME(X);										\
+        PUBNUB_ANALYSIS_ASSUME(X);                                      \
         (X) ? (void)0 : pubnub_assert_failed(#X, __FILE__, __LINE__);   \
     } while (false)
 
@@ -99,6 +99,76 @@
 #define PUBNUB_ASSERT_OPT(X) PUBNUB_UNUSED(X)
 #endif
 
+
+/* The common "rich" macro for types that are compareable
+   and printf-able.
+
+   Users should use the "front-ends" for individual types.
+*/
+#define PUBNUB_ASSERT_RICH_IMPL(T, fmt, a, op, b) do {                  \
+    T M_a_ = (a), M_b_ = (b);                                           \
+    int holds = M_a_ op M_b_;                                           \
+    PUBNUB_ANALYSIS_ASSUME(holds);                                      \
+    if (!holds) {                                                       \
+        char s[300];                                                    \
+        snprintf(s, sizeof s, "`" #a " " #op " " #b "`; `" #a "`=" fmt ", `" #b "`=" fmt, M_a_, M_b_);\
+        pubnub_assert_failed(s, __FILE__, __LINE__);                    \
+    }                                                                   \
+    } while (false)
+
+#define PUBNUB_ASSERT_INT_IMPL(a, op, b) PUBNUB_ASSERT_RICH_IMPL(int, "%d", a, op, b)
+#define PUBNUB_ASSERT_UINT_IMPL(a, op, b) PUBNUB_ASSERT_RICH_IMPL(unsigned, "%u", a, op, b)
+
+/* Define the ASSERT_INT macro for the highest (_EX) level.
+ */
+#if defined PUBNUB_ASSERT_LEVEL_EX
+#define PUBNUB_ASSERT_INT_EX(a, op, b) PUBNUB_ASSERT_INT_IMPL(a, op, b)
+#else
+#define PUBNUB_ASSERT_INT_EX(a, op, b) PUBNUB_UNUSED(a); PUBNUB_UNUSED(b)
+#endif
+
+/* Define the ASSERT_INT macro for the regular level.
+ */
+#if defined(PUBNUB_ASSERT_IS_ACTIVE)
+#define PUBNUB_ASSERT_INT(a, op, b) PUBNUB_ASSERT_INT_IMPL(a, op, b)
+#else
+#define PUBNUB_ASSERT_INT(a, op, b) PUBNUB_UNUSED(a); PUBNUB_UNUSED(b)
+#endif
+
+
+/* Define the ASSERT_INT macro for the lowest level
+ */
+#if !defined(PUBNUB_ASSERT_LEVEL_NONE)
+#define PUBNUB_ASSERT_INT_OPT(a, op, b) PUBNUB_ASSERT_INT_IMPL(a, op, b)
+#else
+#define PUBNUB_ASSERT_INT_OPT(a, op, b) PUBNUB_UNUSED(a); PUBNUB_UNUSED(b)
+#endif
+
+
+/* Define the ASSERT_INT macro for the highest (_EX) level.
+ */
+#if defined PUBNUB_ASSERT_LEVEL_EX
+#define PUBNUB_ASSERT_UINT_EX(a, op, b) PUBNUB_ASSERT_UINT_IMPL(a, op, b)
+#else
+#define PUBNUB_ASSERT_UINT_EX(a, op, b) PUBNUB_UNUSED(a); PUBNUB_UNUSED(b)
+#endif
+
+/* Define the ASSERT_UINT macro for the regular level.
+ */
+#if defined(PUBNUB_ASSERT_IS_ACTIVE)
+#define PUBNUB_ASSERT_UINT(a, op, b) PUBNUB_ASSERT_UINT_IMPL(a, op, b)
+#else
+#define PUBNUB_ASSERT_UINT(a, op, b) PUBNUB_UNUSED(a); PUBNUB_UNUSED(b)
+#endif
+
+
+/* Define the ASSERT_INT macro for the lowest level
+ */
+#if !defined(PUBNUB_ASSERT_LEVEL_NONE)
+#define PUBNUB_ASSERT_UINT_OPT(a, op, b) PUBNUB_ASSERT_UINT_IMPL(a, op, b)
+#else
+#define PUBNUB_ASSERT_UINT_OPT(a, op, b) PUBNUB_UNUSED(a); PUBNUB_UNUSED(b)
+#endif
 
 
 /** This will invoke the installed assert handler.  The default

core/pubnub_internal_common.h
@@ -45,7 +45,9 @@ enum PBSocketState {
     /** All of the data that was to be sent has been sent. */
     STATE_DATA_SENT = 6,
     /** Reading a line */
-    STATE_READ_LINE = 7
+    STATE_READ_LINE = 7,
+    /** Sending data */
+    STATE_SENDING_DATA = 8
 };
 
 
@@ -105,6 +107,18 @@ typedef struct pbntlm_context pbntlm_ctx_t;
     alignment issues when this is included from both C and C++
     compilers, especially pre-C99 C compilers (like MSVC (at least
     until MSVC 2013)).
+
+    Here's a diagram of the "relationship" between the `ptr`,
+    `left` and `unreadlen` with `core.http_buf`:
+
+            ptr
+             |  unreadlen  |    left         |
+             |<----------->|<--------------->|
+             v             |                 |
+    +----------------------------------------+
+    |          core.http_buf                 |
+    +----------------------------------------+
+
 */
 struct pubnub_ {
     struct pbcc_context core;
@@ -114,16 +128,13 @@ struct pubnub_ {
     /** Type of current transaction */
     enum pubnub_trans trans;
 
-    /** Pointer to the next data to be sent. */
-    uint8_t const *sendptr;   
+    /** The number of bytes we got (in our buffer) from network but
+        have not processed yet. */
+    uint16_t unreadlen;         
 
-    /** The number of bytes left to be sent. */
-    uint16_t sendlen;         
-
-    /** The number of bytes left to be read. */
-    uint16_t readlen;         
-
-    /** Pointer to next free byte in the read buffer*/
+    /** Pointer to next byte to read from our buffer or next byte to
+        send in the user-supplied send buffer.
+     */
     uint8_t *ptr;          
 
     /** Number of bytes left (empty) in the read buffer */
@@ -132,7 +143,7 @@ struct pubnub_ {
     /** The state of the socket. */
     enum PBSocketState sock_state;   
 
-    /** Number of bytes to read - given by the user */
+    /** Number of bytes to send or read - given by the user */
     unsigned len;          
 
     /** Indicates whether we are receiving chunked or regular HTTP

core/pubnub_netcore.c
@@ -38,6 +38,7 @@ static void outcome_detected(struct pubnub_ *pb, enum pubnub_res rslt)
 
 static enum pubnub_res dont_parse(struct pbcc_context *p)
 {
+    PUBNUB_UNUSED(p);
     PUBNUB_LOG_ERROR("Parsing not initialized for a type of transaction\n");
     return PNR_INTERNAL_ERROR;
 }
@@ -395,17 +396,17 @@ next_state:
         break;
     case PBS_TX_HOST:
         i = pbpal_send_status(pb);
-        if (i <= 0) {
+        if (i < 0) {
+            outcome_detected(pb, PNR_IO_ERROR);
+        }
+        else if (0 == i) {
             if ((pb->proxy_type == pbproxyHTTP_CONNECT) && !pb->proxy_tunnel_established) {
-                char port_num[20];
-                snprintf(port_num, sizeof port_num, ":%d", 80);
-                pbpal_send_str(pb, port_num);
+                pbpal_send_literal_str(pb, ":80");
                 pb->state = PBS_TX_PORT_NUM;
-                goto next_state;
             }
             else {
                 pb->state = PBS_TX_PATH;
-                if ((i < 0) || (-1 == pbpal_send_str(pb, pb->core.http_buf))) {
+                if (-1 == pbpal_send_str(pb, pb->core.http_buf)) {
                     outcome_detected(pb, PNR_IO_ERROR);
                     break;
                 }
@@ -414,16 +415,6 @@ next_state:
         }
         break;
     case PBS_TX_PORT_NUM:
-        i = pbpal_send_status(pb);
-        if (i <= 0) {
-            pb->state = PBS_TX_PATH;
-            if (i < 0) {
-                outcome_detected(pb, PNR_IO_ERROR);
-                break;
-            }
-            goto next_state;
-        }
-        break;
 #endif /* PUBNUB_PROXY_API */
     case PBS_TX_PATH:
         i = pbpal_send_status(pb);
@@ -459,7 +450,7 @@ next_state:
             if (0 == pbproxy_http_header_to_send(pb, header_to_send+2, sizeof header_to_send-2)) {
                 PUBNUB_LOG_TRACE("Sending HTTP proxy header: '%s'\n", header_to_send);
                 pb->state = PBS_TX_PROXY_AUTHORIZATION;
-                if ((i < 0) || (-1 == pbpal_send_str(pb, header_to_send))) {
+                if (-1 == pbpal_send_str(pb, header_to_send)) {
                     outcome_detected(pb, PNR_IO_ERROR);
                     break;
                 }
@@ -537,7 +528,7 @@ next_state:
             char h_chunked[] = "Transfer-Encoding: chunked";
             char h_length[] = "Content-Length: ";
             int read_len = pbpal_read_len(pb);
-            PUBNUB_LOG_TRACE("pb=%p header line was read: %.*s\n", pb, read_len, pb->core.http_buf);
+            PUBNUB_LOG_TRACE("pb=%p header line was read: '%.*s'\n", pb, read_len, pb->core.http_buf);
             WATCH_INT(read_len);
             if (read_len <= 2) {
                 pb->core.http_buf_len = 0;
@@ -578,6 +569,15 @@ next_state:
             pb->state = PBS_RX_HEADERS;
             goto next_state;
         }
+        case PNR_TX_BUFF_TOO_SMALL:
+            /** We could try to copy the "line so far" to the reply
+                buffer and then do some processing there, but, it
+                could become very complex, as we could have a few more
+                of these until we finally find a newline. So, for now,
+                just try to recover by skipping this line.
+            */
+            pb->state = PBS_RX_HEADERS;
+            goto next_state;
         default:
             outcome_detected(pb, pbrslt);
             break;

lib/sockets/pbpal_sockets.c
@@ -16,7 +16,7 @@
 static void buf_setup(pubnub_t *pb)
 {
     pb->ptr = (uint8_t*)pb->core.http_buf;
-    pb->left = sizeof pb->core.http_buf;
+    pb->left = sizeof pb->core.http_buf / sizeof pb->core.http_buf[0];
 }
 
 
@@ -43,23 +43,18 @@ void pbpal_init(pubnub_t *pb)
     pal_init();
     pb->pal.socket = SOCKET_INVALID;
     pb->sock_state = STATE_NONE;
-    pb->readlen = 0;
     buf_setup(pb);
 }
 
 
 int pbpal_send(pubnub_t *pb, void const *data, size_t n)
 {
-    if (n == 0) {
-        return 0;
-    }
-    if (pb->sock_state != STATE_NONE) {
-        PUBNUB_LOG_ERROR("pbpal_send(): pb->sock_state != STATE_NONE (=%d)\n", pb->sock_state);
-        return -1;
-    }
-    pb->sendptr = (uint8_t*)data;
-    pb->sendlen = (uint16_t)n;
-    pb->sock_state = STATE_NONE;
+    PUBNUB_ASSERT_INT_OPT(pb->sock_state, ==, STATE_NONE);
+
+    pb->ptr = (uint8_t*)data;
+    pb->len = (uint16_t)n;
+    pb->sock_state = STATE_SENDING_DATA;
+    pb->left = sizeof pb->core.http_buf / sizeof pb->core.http_buf[0];
 
     return pbpal_send_status(pb);
 }
@@ -71,58 +66,10 @@ int pbpal_send_str(pubnub_t *pb, char const *s)
 }
 
 
-int pbpal_send_status(pubnub_t *pb)
-{
-    int r;
-    if (0 == pb->sendlen) {
-        return 0;
-    }
-    r = socket_send(pb->pal.socket, (char*)pb->sendptr, pb->sendlen, 0);
-    if (r < 0) {
-        return socket_would_block() ? +1 : -1;
-    }
-    if (r > pb->sendlen) {
-        PUBNUB_LOG_WARNING("That's some over-achieving socket!\n");
-        r = pb->sendlen;
-    }
-    pb->sendptr += r;
-    pb->sendlen -= r;
-
-    return (0 == pb->sendlen) ? 0 : +1;
-}
-
-
-int pbpal_start_read_line(pubnub_t *pb)
-{
-    if (pb->sock_state != STATE_NONE) {
-        PUBNUB_LOG_ERROR("pbpal_start_read_line(): pb->sock_state != STATE_NONE: "); WATCH_ENUM(pb->sock_state);
-        return -1;
-    }
-
-    if (pb->ptr > (uint8_t*)pb->core.http_buf) {
-        unsigned distance = pb->ptr - (uint8_t*)pb->core.http_buf;
-        PUBNUB_ASSERT_OPT(pb->ptr + pb->left == pb->core.http_buf + PUBNUB_BUF_MAXLEN);
-        memmove(pb->core.http_buf, pb->ptr, pb->readlen);
-        pb->ptr -= distance;
-        pb->left += distance;
-    }
-    else {
-        if (pb->left == 0) {
-            /* Obviously, our buffer is not big enough, maybe some
-               error should be reported */
-            buf_setup(pb);
-        }
-    }
-
-    pb->sock_state = STATE_READ_LINE;
-    return +1;
-}
-
-
 static void report_error_from_environment(pubnub_t* pb)
 {
     char const* err_str;
-                
+
 #if HAVE_STRERROR_R
     char errstr_r[1024];
     strerror_r(errno, errstr_r, sizeof errstr_r / sizeof errstr_r[0]);
@@ -134,34 +81,90 @@ static void report_error_from_environment(pubnub_t* pb)
 #else
     err_str = strerror(errno);
 #endif
-    PUBNUB_LOG_TRACE("pbpal_line_read_status(pb=%p): errno=%d('%s') use_blocking_io=%d\n", pb, errno, err_str, pb->options.use_blocking_io);
+    PUBNUB_LOG_DEBUG("pbpal_line_read_status(pb=%p): errno=%d('%s') use_blocking_io=%d\n",
+                     pb, errno, err_str, pb->options.use_blocking_io);
 #if defined(_WIN32)
-    PUBNUB_LOG_TRACE("pbpal_line_read_status(pb=%p): GetLastErrror()=%d WSAGetLastError()=%d\n", 
-                     pb, GetLastError(), WSAGetLastError()
-        );
+    PUBNUB_LOG_DEBUG("pbpal_line_read_status(pb=%p): GetLastErrror()=%lu WSAGetLastError()=%d\n",
+                     pb, GetLastError(), WSAGetLastError());
 #endif
 }
 
 
-static enum pubnub_res pbrslt_from_socket_error(int socket_result, pubnub_t *pb)
+static enum pubnub_res handle_socket_error(int socket_result, pubnub_t *pb)
 {
-    PUBNUB_ASSERT_OPT(socket_result <= 0);
+    PUBNUB_ASSERT_INT_OPT(socket_result, <=, 0);
     if (socket_result < 0) {
         if (socket_would_block()) {
             if (PUBNUB_BLOCKING_IO_SETTABLE && pb->options.use_blocking_io) {
+                pb->sock_state = STATE_NONE;
                 return PNR_TIMEOUT;
             }
             return PNR_IN_PROGRESS;
         }
         else {
+            pb->sock_state = STATE_NONE;
             report_error_from_environment(pb);
             return socket_timed_out() ? PNR_CONNECTION_TIMEOUT : PNR_IO_ERROR;
         }
     }
     else if (0 == socket_result) {
+        pb->sock_state = STATE_NONE;
         return PNR_TIMEOUT;
     }
+    pb->sock_state = STATE_NONE;
     return PNR_INTERNAL_ERROR;
+}
+
+
+int pbpal_send_status(pubnub_t *pb)
+{
+    int rslt;
+
+    if (0 == pb->len) {
+        return 0;
+    }
+
+    PUBNUB_ASSERT_OPT(pb->sock_state == STATE_SENDING_DATA);
+
+    rslt = socket_send(pb->pal.socket, (char*)pb->ptr, pb->len, 0);
+    if (rslt <= 0) {
+        rslt = (handle_socket_error(rslt, pb) == PNR_IN_PROGRESS) ? +1 : -1;
+    }
+    else {
+        PUBNUB_ASSERT_OPT((unsigned)rslt <= pb->len);
+        pb->ptr += rslt;
+        pb->len -= rslt;
+        rslt = (0 == pb->len) ? 0 : +1;
+    }
+
+    if (rslt <= 0) {
+        pb->ptr = (uint8_t*)pb->core.http_buf;
+        pb->unreadlen = 0;
+        pb->sock_state = STATE_NONE;
+    }
+
+    return rslt;
+}
+
+
+int pbpal_start_read_line(pubnub_t *pb)
+{
+    unsigned distance;
+
+    PUBNUB_ASSERT_INT_OPT(pb->sock_state, ==, STATE_NONE);
+
+    if (pb->unreadlen > 0) {
+        PUBNUB_ASSERT_OPT(pb->ptr + pb->unreadlen <= pb->core.http_buf + PUBNUB_BUF_MAXLEN);
+        memmove(pb->core.http_buf, pb->ptr, pb->unreadlen);
+    }
+    distance = pb->ptr - (uint8_t*)pb->core.http_buf;
+    PUBNUB_ASSERT_UINT(distance + pb->left + pb->unreadlen, ==, sizeof pb->core.http_buf / sizeof pb->core.http_buf[0]);
+    pb->ptr -= distance;
+    pb->left += distance;
+
+    pb->sock_state = STATE_READ_LINE;
+
+    return +1;
 }
 
 
@@ -169,45 +172,36 @@ enum pubnub_res pbpal_line_read_status(pubnub_t *pb)
 {
     uint8_t c;
 
-    if (pb->readlen == 0) {
+    PUBNUB_ASSERT_OPT(STATE_READ_LINE == pb->sock_state);
+
+    if (pb->unreadlen == 0) {
         int recvres;
         PUBNUB_ASSERT_OPT(pb->ptr + pb->left == pb->core.http_buf + PUBNUB_BUF_MAXLEN);
         recvres = socket_recv(pb->pal.socket, (char*)pb->ptr, pb->left, 0);
         if (recvres <= 0) {
-            return pbrslt_from_socket_error(recvres, pb);
+            return handle_socket_error(recvres, pb);
         }
+        PUBNUB_ASSERT_OPT(recvres <= pb->left);
         PUBNUB_LOG_TRACE("pb=%p have new data of length=%d: %.*s\n", pb, recvres, recvres, pb->ptr);
-        pb->sock_state = STATE_READ_LINE;
-        pb->readlen = recvres;
+        pb->unreadlen = recvres;
+        pb->left -= recvres;
     }
 
-    while (pb->left > 0 && pb->readlen > 0) {
-        c = *pb->ptr++;
+    while (pb->unreadlen > 0) {
+        --pb->unreadlen;
 
-        --pb->readlen;
-        --pb->left;
-        
+        c = *pb->ptr++;
         if (c == '\n') {
-            int read_len = pbpal_read_len(pb);
-            PUBNUB_LOG_TRACE("\n found: "); WATCH_INT(read_len); WATCH_USHORT(pb->readlen);
+            PUBNUB_LOG_TRACE("pb=%p, newline found, line length: %d, ", pb, pbpal_read_len(pb)); WATCH_USHORT(pb->unreadlen);
             pb->sock_state = STATE_NONE;
             return PNR_OK;
         }
     }
 
     if (pb->left == 0) {
-        /* Buffer has been filled, but new-line char has not been
-         * found.  We have to "reset" this "mini-fsm", as otherwise we
-         * won't read anything any more. This means that we have lost
-         * the current contents of the buffer, which is bad. In some
-         * general code, that should be reported, as the caller could
-         * save the contents of the buffer somewhere else or simply
-         * decide to ignore this line (when it does end eventually).
-         */
+        PUBNUB_LOG_ERROR("pbpal_line_read_status(pb=%p): buffer full but newline not found", pb);
         pb->sock_state = STATE_NONE;
-    }
-    else {
-        pb->sock_state = STATE_NEWDATA_EXHAUSTED;
+        return PNR_TX_BUFF_TOO_SMALL;
     }
 
     return PNR_IN_PROGRESS;
@@ -216,86 +210,66 @@ enum pubnub_res pbpal_line_read_status(pubnub_t *pb)
 
 int pbpal_read_len(pubnub_t *pb)
 {
-    return sizeof pb->core.http_buf - pb->left;
+    return (char*)pb->ptr - pb->core.http_buf;
 }
 
 
 int pbpal_start_read(pubnub_t *pb, size_t n)
 {
-    if (pb->sock_state != STATE_NONE) {
-        PUBNUB_LOG_ERROR("pbpal_start_read(): pb->sock_state != STATE_NONE: "); WATCH_ENUM(pb->sock_state);
-        return -1;
+    unsigned distance;
+
+    PUBNUB_ASSERT_UINT_OPT(n, >, 0);
+    PUBNUB_ASSERT_INT_OPT(pb->sock_state, ==, STATE_NONE);
+
+    WATCH_USHORT(pb->unreadlen);
+    WATCH_USHORT(pb->left);
+    if (pb->unreadlen > 0) {
+        PUBNUB_ASSERT_OPT(pb->ptr + pb->unreadlen <= pb->core.http_buf + PUBNUB_BUF_MAXLEN);
+        memmove(pb->core.http_buf, pb->ptr, pb->unreadlen);
     }
-    if (pb->ptr > (uint8_t*)pb->core.http_buf) {
-        unsigned distance = pb->ptr - (uint8_t*)pb->core.http_buf;
-        PUBNUB_ASSERT_OPT(pb->ptr + pb->readlen <= pb->core.http_buf + PUBNUB_BUF_MAXLEN);
-        memmove(pb->core.http_buf, pb->ptr, pb->readlen);
-        pb->ptr -= distance;
-        pb->left += distance;
-    }
-    else {
-        if (pb->left == 0) {
-            PUBNUB_LOG_ERROR("pbpal_start_read(pb=%p) pb->left=0, dismissing old buffer\n", pb);
-            buf_setup(pb);
-        }
-    }
+    distance = pb->ptr - (uint8_t*)pb->core.http_buf;
+    WATCH_UINT(distance);
+    PUBNUB_ASSERT_UINT(distance + pb->unreadlen + pb->left, ==, sizeof pb->core.http_buf / sizeof pb->core.http_buf[0]);
+    pb->ptr -= distance;
+    pb->left += distance;
+
     pb->sock_state = STATE_READ;
     pb->len = n;
+
     return +1;
 }
 
 
 enum pubnub_res pbpal_read_status(pubnub_t *pb)
 {
-    unsigned to_read = 0;
-    WATCH_ENUM(pb->sock_state);
-    WATCH_USHORT(pb->readlen);
-    WATCH_USHORT(pb->left);
-    WATCH_UINT(pb->len);
+    int have_read;
 
-    if (pb->readlen == 0) {
-        int recvres;
-        to_read =  pb->len;
-        if (to_read > pb->left) {
-            to_read = pb->left;
+    PUBNUB_ASSERT_OPT(STATE_READ == pb->sock_state);
+
+    if (0 == pb->unreadlen) {
+        unsigned to_recv = pb->len;
+        if (to_recv > pb->left) {
+            to_recv = pb->left;
         }
-        recvres = socket_recv(pb->pal.socket, (char*)pb->ptr, to_read, 0);
-        if (recvres <= 0) {
-            return pbrslt_from_socket_error(recvres, pb);
+        PUBNUB_ASSERT_OPT(to_recv > 0 );
+        have_read = socket_recv(pb->pal.socket, (char*)pb->ptr, to_recv, 0);
+        if (have_read <= 0) {
+            return handle_socket_error(have_read, pb);
         }
-        pb->sock_state = STATE_READ;
-        pb->readlen = recvres;
-    } 
-
-
-    to_read = pb->len;
-    if (pb->readlen < to_read) {
-        to_read = pb->readlen;
-    }
-    pb->ptr += to_read;
-    pb->readlen -= to_read;
-    PUBNUB_ASSERT_OPT(pb->left >= to_read);
-    pb->left -= to_read;
-    pb->len -= to_read;
-
-    if (pb->len == 0) {
-        pb->sock_state = STATE_NONE;
-        return PNR_OK;
-    }
-
-    if (pb->left == 0) {
-        /* Buffer has been filled, but the requested block has not been
-         * read.  We have to "reset" this "mini-fsm", as otherwise we
-         * won't read anything any more. This means that we have lost
-         * the current contents of the buffer, which is bad. In some
-         * general code, that should be reported, as the caller could
-         * save the contents of the buffer somewhere else or simply
-         * decide to ignore this block (when it does end eventually).
-         */
-        pb->sock_state = STATE_NONE;
+        PUBNUB_ASSERT_OPT(pb->left >= have_read);
+        pb->left -= have_read;
     }
     else {
-        pb->sock_state = STATE_NEWDATA_EXHAUSTED;
+        have_read = (pb->unreadlen >= pb->len) ? pb->len : pb->unreadlen;
+        pb->unreadlen -= have_read;
+    }
+
+    pb->len -= have_read;
+    pb->ptr += have_read;
+
+    if ((0 == pb->len) || (0 == pb->left)) {
+        pb->sock_state = STATE_NONE;
+        return PNR_OK;
     }
 
     return PNR_IN_PROGRESS;
@@ -317,7 +291,7 @@ void pbpal_forget(pubnub_t *pb)
 
 int pbpal_close(pubnub_t *pb)
 {
-    pb->readlen = 0;
+    pb->unreadlen = 0;
     if (pb->pal.socket != SOCKET_INVALID) {
         pbntf_lost_socket(pb, pb->pal.socket);
         socket_close(pb->pal.socket);

windows/windows.mk
@@ -2,7 +2,7 @@ SOURCEFILES = ..\core\pubnub_pubsubapi.c ..\core\pubnub_coreapi.c ..\core\pubnub
 
 OBJFILES = pubnub_pubsubapi.obj pubnub_coreapi.obj pubnub_ccore_pubsub.obj pubnub_ccore.obj pubnub_netcore.obj pbpal_sockets.obj pbpal_resolv_and_connect_sockets.obj pubnub_alloc_std.obj pubnub_assert_std.obj pubnub_generate_uuid.obj pubnub_blocking_io.obj pbbase64.obj pubnub_timers.obj pubnub_json_parse.obj pubnub_proxy.obj pubnub_proxy_core.obj pbhttp_digest.obj md5.obj pbntlm_core.obj pbntlm_packer_sspi.obj pubnub_set_proxy_from_system_windows.obj pubnub_helper.obj pubnub_version_windows.obj pubnub_generate_uuid_windows.obj pbpal_windows_blocking_io.obj snprintf.obj
 
-CFLAGS = /Zi /MP /W3  /D PUBNUB_THREADSAFE /D HAVE_STRERROR_S
+CFLAGS = /Zi /MP /W3  /D PUBNUB_THREADSAFE /D PUBNUB_LOG_LEVEL=PUBNUB_LOG_LEVEL_WARNING /D HAVE_STRERROR_S
 # /Zi enables debugging, remove to get a smaller .exe and no .pdb
 # /MP uses one compiler (`cl`) process for each input file, enabling faster build
 