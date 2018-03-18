c1829186b65b610803863a5d50bf481c422573ae
Vladimir Veljkovic <vl.veljkovic@yahoo.com>
2018-03-18 17:11:10 +0100

    Add the module for setting HTTP Keep-Alive options

---
+++ core/pubnub_keep_alive.c
@@ -0,0 +1,9 @@
+/* -*- c-file-style:"stroustrup"; indent-tabs-mode: nil -*- */
+#include "pubnub_keep_alive.h"
+
+
+void pubnub_set_keep_alive_param(pubnub_t* pb, unsigned timeout, unsigned max)
+{
+    pb->keep_alive.timeout = timeout;
+    pb->keep_alive.max     = max;
+}
