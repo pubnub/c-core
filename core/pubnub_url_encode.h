23e41670ea0d63039c4217c222d7d4def73156b6
650elx <bprokop02@gmail.com>
2021-12-13 12:07:14 +0100

    feat(PAM): change URL encoding to match PN standards

--- core/pubnub_url_encode.h
+++ core/pubnub_url_encode.h
@@ -4,8 +4,8 @@
 
 /* RFC 3986 Unreserved characters plus few
  * safe reserved ones. */
-#define OK_SPAN_CHARACTERS "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.~,=;@[]"
-#define OK_SPAN_CHARS_MINUS_COMMA "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.~=;@[]"
+#define OK_SPAN_CHARACTERS "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.~[]"
+#define OK_SPAN_CHARS_MINUS_COMMA "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.~[]"
 
 /** Url-encodes string @p what to user provided @p buffer which has its @p buffer_size.
     @retval length of url-encoded string on success,
@@ -14,4 +14,3 @@
 int pubnub_url_encode(char* buffer, char const* what, size_t buffer_size, enum pubnub_trans pt);
 
 #endif /* !defined INC_PUBNUB_URL_ENCODE */
-
