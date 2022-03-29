1c5f8708202e1d28c237c9411026c515998e5f06
Pandu Masabathula <budgetpreneur@users.noreply.github.com>
2022-03-30 00:02:38 +0530

    cast to char*

--- core/pubnub_coreapi.c
+++ core/pubnub_coreapi.c
@@ -307,7 +307,7 @@ enum pubnub_res pubnub_set_state(pubnub_t*   pb,
             if (channel) {
                 char* chan_token = strtok((char*)channel, ",");
                 while( chan_token != NULL ) {
-                    if (' ' != chan_token){
+                    if ((char*)" " != chan_token){
                         sprintf(json_state, "%s%s\"%s\":%s", json_state, (ch_cnt > 0) ? "," : "", chan_token, state);
                         ch_cnt++;
                     }
@@ -317,7 +317,7 @@ enum pubnub_res pubnub_set_state(pubnub_t*   pb,
             if (channel_group) {
                 char* cg_token = strtok((char*)channel_group, ",");
                 while( cg_token != NULL ) {
-                    if (' ' != cg_token){
+                    if ((char*)" " != cg_token){
                         sprintf(json_state, "%s%s\"%s\":%s", json_state, (cg_cnt > 0 || ch_cnt > 0) ? "," : "", cg_token, state);
                         cg_cnt++;
                     }
