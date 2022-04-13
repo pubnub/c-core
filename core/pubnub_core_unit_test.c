28789d97abd66d73beeed7dc0bba89c7fb746eb6
Lukasz Klich <lukasz.klich@pubnub.com>
2022-04-13 11:45:18 -0500

    Two expectations

--- core/pubnub_core_unit_test.c
+++ core/pubnub_core_unit_test.c
@@ -2033,13 +2033,14 @@ Ensure(single_context_pubnub, set_state_in_progress)
         "/v2/presence/sub-key/sub-one/channel/ch/uuid/blackbeard/"
         "data?pnsdk=unit-test-0.1&state=%7B%22the_pirate%22%3A%22true%22%7D");
     incoming("HTTP/1.1 200\r\n", NULL);
-    //incoming("", NULL);
-    // incoming("HTTP/1.1 200\r\nContent-Length: "
-    //          "64\r\n\r\n{\"status\":200,\"message\":\"OK\",\"service\":"
-    //          "\"Presence\",\"payload\":{I}}",
-    //          NULL);
+    incoming("", NULL);
     attest(pubnub_set_state(pbp, "ch", NULL, "blackbeard", "{\"the_pirate\":\"true\"}"),
            equals(PNR_STARTED));
+    expect_outgoing_with_url(
+        "/v2/presence/sub-key/sub-one/channel/ch/uuid/blackbeard/"
+        "data?pnsdk=unit-test-0.1&state=%7B%22the_pirate%22%3A%22arrrrrrr_arrrrr%22%7D");
+    incoming("HTTP/1.1 200\r\n", NULL);
+    incoming("", NULL);
     attest(pubnub_set_state(
                pbp, "ch", NULL, "blackbeard", "{\"the_pirate\":\"arrrrrrr_arrrrr\"}"),
            equals(PNR_IN_PROGRESS));
