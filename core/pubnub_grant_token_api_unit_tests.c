8fd78d4bcff41e586e193ba57cd0c8f33a989067
Xavrax <matedahlke@gmail.com>
2022-12-13 04:40:10 +0100

    crashing test cases

--- core/pubnub_grant_token_api_unit_tests.c
+++ core/pubnub_grant_token_api_unit_tests.c
@@ -35,28 +35,50 @@ typedef struct TestCase{
     char* expected_parsed_token;
 } TestCase;
 
-static TestCase test_cases[] = {
+static TestCase parsing_test_cases[] = {
     {
         .raw_token = "p0F2AkF0GmOX59NDdHRsGDxDcmVzpURjaGFuomRteWNoGB9raGVsbG9fd29ybGQDQ2dycKJkbXljZxgfbWNoYW5uZWwtZ3JvdXAHQ3NwY6FlbXlzcGMYH0N1c3KhZm15dXNlchgfRHV1aWSgQ3BhdKVEY2hhbqBDZ3JwoENzcGOhYl4kAUN1c3KhYl4kAUR1dWlkoERtZXRhoENzaWdYIJAZo8ma70ti67jei4f1ytynBcNPGm2lMS_tNuEChca8", 
         .expected_parsed_token = "{\"v\":2, \"t\":1670899667, \"ttl\":60, \"res\":{\"chan\":{\"mych\":31, \"hello_world\":3}, \"grp\":{\"mycg\":31, \"channel-group\":7}, \"spc\":{\"myspc\":31}, \"usr\":{\"myuser\":31}, \"uuid\":{}}, \"pat\":{\"chan\":{}, \"grp\":{}, \"spc\":{\"^$\":1}, \"usr\":{\"^$\":1}, \"uuid\":{}}, \"meta\":{}, \"sig\":\"kBmjyZrvS2LruN6Lh/XK3KcFw08abaUxL+024QKFxrw=\"}"
+    },
+    {
+        .raw_token = "p0F2AkF0GmOX8A9DdHRsGDxDcmVzpURjaGFuomtoZWxsb193b3JsZANkbXljaBgfQ2dycKJtY2hhbm5lbC1ncm91cANkbXljZxgfQ3NwY6FlbXlzcGMYH0N1c3KhZml0c19tZRgcRHV1aWSgQ3BhdKVEY2hhbqBDZ3JwoENzcGOhYl4kAUN1c3KhYl4kAUR1dWlkoERtZXRhoENzaWdYILD4I0SjS4tdpUiOj987_LnuFRQCjbI2UtmlSdvUiWxQ",
+        .expected_parsed_token = "{\"v\":2, \"t\":1670901775, \"ttl\":60, \"res\":{\"chan\":{\"hello_world\":3, \"mych\":31}, \"grp\":{\"channel-group\":3, \"mycg\":31}, \"spc\":{\"myspc\":31}, \"usr\":{\"its_me\":28}, \"uuid\":{}}, \"pat\":{\"chan\":{}, \"grp\":{}, \"spc\":{\"^$\":1}, \"usr\":{\"^$\":1}, \"uuid\":{}}, \"meta\":{}, \"sig\":\"sPgjRKNLi12lSI6P3zv8ue4VFAKNsjZS2aVJ29SJbFA=\"}"
     }
 };
 
 Ensure(token_parsing, should_properly_parse_tokens) {
-    size_t test_cases_count = sizeof(test_cases)/sizeof(test_cases[0]);
-    for (int i = 0; i < test_cases_count; i++) {
-    	char* raw_token = test_cases[0].raw_token;
+    size_t test_cases_count = sizeof(parsing_test_cases)/sizeof(parsing_test_cases[0]);
+
+    for (int test_case = 0; test_case < test_cases_count; test_case++) {
+    	char* raw_token = parsing_test_cases[test_case].raw_token;
     
     	char* parsed_token = pubnub_parse_token(pbp, raw_token);
     
-    	char* expected_parsed_token = test_cases[0].expected_parsed_token;
+    	char* expected_parsed_token = parsing_test_cases[test_case].expected_parsed_token;
     
         assert_that(*parsed_token, is_equal_to(*expected_parsed_token));
+        free(parsed_token);
     }
 }
 
+static char* crashing_test_cases[] = {
+    "dummy data",
+    "abcdefg",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+    "",
+    "1234567890"
+};
+
 Ensure(token_parsing, should_not_crashing_for_not_valid_values) {
-    assert_that(pubnub_parse_token(pbp, "dummy data"), is_null);
+    size_t test_cases_count = sizeof(crashing_test_cases)/sizeof(crashing_test_cases[0]);
+
+    for (int test_case = 0; test_case < test_cases_count; test_case++) {
+        char* parsed_token = pubnub_parse_token(pbp, crashing_test_cases[test_case]);
+
+        assert_that(parsed_token, is_null);
+
+        free(parsed_token);
+    }
 }
 
 #if 0
