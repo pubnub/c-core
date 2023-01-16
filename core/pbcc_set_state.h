2c937d21f532b04380e48a90d973e396053dc1f9
Xavrax <matedahlke@gmail.com>
2023-01-16 09:36:25 +0100

    commit for recheck

--- core/pbcc_set_state.h
+++ core/pbcc_set_state.h
@@ -5,6 +5,7 @@
 #include "pubnub_api_types.h"
 
 /** Adjusting the @pb state after the call to the pubnub services
+    that afects the presence state 
   */
 void pbcc_adjust_state(pubnub_t* pb,
                         char const* channel,
