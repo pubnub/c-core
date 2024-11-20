1c4edfa469f8070b7d8023784c16dc55322a3b34
Serhii Mamontov <sergey@pubnub.com>
2024-11-20 22:04:35 +0200

    test(debug): package config debug

--- .github/workflows/composite/unit-integration-test-runner/run-unit-tests.sh
+++ .github/workflows/composite/unit-integration-test-runner/run-unit-tests.sh
@@ -4,63 +4,69 @@ set -e
 #ls /usr/include/openssl
 #ls /usr/lib/x86_64-linux-gnu/libssl.so
 
-if [ ! -f /usr/lib/x86_64-linux-gnu/pkgconfig/openssl.pc ]; then
-  echo "'openssl' package config not found!"
-else
-  echo "'openssl' package configuration exists"
-  cat /usr/lib/x86_64-linux-gnu/pkgconfig/openssl.pc
+echo "~~~> $1"
+
+if [[ "$1" == "ubuntu" ]]; then
+  if [ ! -f /usr/lib/x86_64-linux-gnu/pkgconfig/openssl.pc ]; then
+    echo "~~~> 'openssl' package config not found!"
+  else
+    echo "~~~> 'openssl' package configuration exists"
+    cat /usr/lib/x86_64-linux-gnu/pkgconfig/openssl.pc
+    echo "----"
+  fi
   echo "----"
-fi
-echo "----"
-if [ ! -f /usr/lib/x86_64-linux-gnu/pkgconfig/libssl.pc ]; then
-    echo "'libssl' package config not found!"
-else
-  echo "'libssl' package configuration exists"
-  cat /usr/lib/x86_64-linux-gnu/pkgconfig/libssl.pc
-  echo "----"
+  if [ ! -f /usr/lib/x86_64-linux-gnu/pkgconfig/libssl.pc ]; then
+      echo "~~~> 'libssl' package config not found!"
+  else
+    echo "~~~> 'libssl' package configuration exists"
+    cat /usr/lib/x86_64-linux-gnu/pkgconfig/libssl.pc
+    echo "----"
+
+    if [ ! -f /usr/lib/x86_64-linux-gnu/pkgconfig/openssl.pc ]; then
+      echo "~~~> 'openssl' package config not found!"
+      sudo ln -s /usr/lib/x86_64-linux-gnu/pkgconfig/libssl.pc /usr/lib/x86_64-linux-gnu/pkgconfig/openssl.pc
+    else
+      echo "~~~> 'openssl' package configuration exists"
+    fi
+  fi
+
 
   if [ ! -f /usr/lib/x86_64-linux-gnu/pkgconfig/openssl.pc ]; then
-    echo "'openssl' package config not found!"
-    sudo ln -s /usr/lib/x86_64-linux-gnu/pkgconfig/libssl.pc /usr/lib/x86_64-linux-gnu/pkgconfig/openssl.pc
+    OPENSSL_VERSION=$(openssl version | awk '{print $2}')
+    echo "prefix=/usr
+  exec_prefix=\${prefix}
+  libdir=\${exec_prefix}/lib/x86_64-linux-gnu
+  includedir=\${prefix}/include
+
+  Name: OpenSSL
+  Description: Secure Sockets Layer and cryptography libraries and tools
+  Version: $OPENSSL_VERSION
+  Requires:
+  Libs: -L\${libdir} -lssl -lcrypto
+  Cflags: -I\${includedir}" | sudo tee /usr/lib/x86_64-linux-gnu/pkgconfig/openssl.pc > /dev/null
   else
-    echo "'openssl' package configuration exists"
+    OPENSSL_PC_FILE=/usr/lib/x86_64-linux-gnu/pkgconfig/openssl.pc
+  # Fix Libs if required.
+  if ! grep -q "Libs:" "$OPENSSL_PC_FILE"; then
+    echo "~~~> Adding missing Libs field..."
+    echo "Libs: -L/usr/lib/x86_64-linux-gnu -lssl -lcrypto" | sudo tee -a "$OPENSSL_PC_FILE"
   fi
+
+  # Fix C flags if required.
+  if ! grep -q "Cflags:" "$OPENSSL_PC_FILE"; then
+    echo "~~~> Adding missing Cflags field..."
+    echo "Cflags: -I/usr/include" | sudo tee -a "$OPENSSL_PC_FILE"
+  fi
+    echo "----"
+    echo "$OPENSSL_PC_FILE"
+    cat "$OPENSSL_PC_FILE"
+    echo "----"
+  fi
+
+  export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
+  echo "CFLAGS: '$(pkg-config --cflags openssl)'"
+  echo "LIBS: '$(pkg-config --libs openssl)'"
 fi
-
-
-if [ ! -f /usr/lib/x86_64-linux-gnu/pkgconfig/openssl.pc ]; then
-  OPENSSL_VERSION=$(openssl version | awk '{print $2}')
-  echo "prefix=/usr
-exec_prefix=\${prefix}
-libdir=\${exec_prefix}/lib/x86_64-linux-gnu
-includedir=\${prefix}/include
-
-Name: OpenSSL
-Description: Secure Sockets Layer and cryptography libraries and tools
-Version: $OPENSSL_VERSION
-Requires:
-Libs: -L\${libdir} -lssl -lcrypto
-Cflags: -I\${includedir}" | sudo tee /usr/lib/x86_64-linux-gnu/pkgconfig/openssl.pc > /dev/null
-else
-  OPENSSL_PC_FILE=/usr/lib/x86_64-linux-gnu/pkgconfig/openssl.pc;
-# Fix Libs if required.
-if ! grep -q "Libs:" "$OPENSSL_PC_FILE"; then
-  echo "Adding missing Libs field..."
-  echo "Libs: -L/usr/lib/x86_64-linux-gnu -lssl -lcrypto" | sudo tee -a "$OPENSSL_PC_FILE"
-fi
-
-# Fix C flags if required.
-if ! grep -q "Cflags:" "$OPENSSL_PC_FILE"; then
-  echo "Adding missing Cflags field..."
-  echo "Cflags: -I/usr/include" | sudo tee -a "$OPENSSL_PC_FILE"
-fi
-fi
-
-
-export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
-echo "CFLAGS: '$(pkg-config --cflags openssl)'"
-echo "LIBS: '$(pkg-config --libs openssl)'"
-
 
 echo "::group::Run unit tests ('$1' $CC / $CXX)"
 cd "$GITHUB_WORKSPACE/core"
