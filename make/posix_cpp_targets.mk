052ce4372b30897ba7a1023a60cb5919682936b7
Serhii Mamontov <sergey@pubnub.com>
2024-12-10 02:51:05 +0200

    test: add missing target for acceptance tests

--- make/posix_cpp_targets.mk
+++ make/posix_cpp_targets.mk
@@ -18,6 +18,19 @@ endif
 
 # ----------------- Samples based on sync PubNub library -----------------
 
+#openssl/pubnub_sync : $(SOURCEFILES) $(SYNC_INTF_SOURCEFILES) pubnub_futres_sync.cpp
+#	$(CXX) -c $(CFLAGS) -x c++ $(SOURCEFILES) $(SYNC_INTF_SOURCEFILES) pubnub_futres_sync.cpp
+#	ar rcs pubnub_sync.a $(OBJFILES) $(SYNC_INTF_OBJFILES) pubnub_futres_sync.o
+
+ifeq ($(OPENSSL), 1)
+    $(TARGET_BUILD_PATH)pubnub_sync$(APP_EXT): \
+        $(SOURCE_FILES)                  \
+        $(SYNC_SOURCE_FILES)             \
+        $(PUBNUB_FUTRES_SYNC_SOURCE_FILE)
+		$(COMPILER) -c $(COMPILER_FLAGS) $(CPPFLAGS) $(PREREQUISITES)
+		$(LIB_TOOL) $(LIB_CMD) $(LIB_OUT_FLAG)$@ $(OBJECT_FILES) $(SYNC_OBJECT_FILES)
+endif
+
 $(TARGET_BUILD_PATH)pubnub_sync_subloop_sample$(APP_EXT): \
     ../cpp/samples/pubnub_subloop_sample.cpp \
     $(SOURCE_FILES)                          \
