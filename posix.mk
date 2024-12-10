31b23ec3d43c888c6706fed423cefa87a2cadf62
Serhii Mamontov <sergey@pubnub.com>
2024-12-10 02:27:12 +0200

    test: fix cpp openssl target name

--- posix.mk
+++ posix.mk
@@ -4,7 +4,7 @@ all:
 	make -C posix -f posix.mk pubnub_fntest
 	make -C openssl -f posix.mk pubnub_fntest
 	make -C cpp -f posix.mk fntest_runner
-	make -C cpp -f posix_openssl.mk openssl/pubnub_fntest
+	make -C cpp -f posix_openssl.mk openssl/fntest_runner
 
 unittest:
 	make -C core
