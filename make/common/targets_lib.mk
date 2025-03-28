8fb7c39ae0e2ef072fb1658acb6cfa2a1c4ad655
Xavrax <matedahlke@gmail.com>
2025-03-28 12:16:51 +0100

    add enforcement to makefiles but not working :c

--- make/common/targets_lib.mk
+++ make/common/targets_lib.mk
@@ -56,3 +56,40 @@ pubnub_callback$(LIB_EXT): $(SOURCE_FILES) $(CALLBACK_SOURCE_FILES)
 pubnub_callback_dynamiciv$(LIB_EXT): $(SOURCE_FILES) $(CALLBACK_SOURCE_FILES)
 	$(COMPILER) -c $(COMPILER_FLAGS) $(CALLBACK_CPPFLAGS) -D PUBNUB_RAND_INIT_VECTOR=1 $(PREREQUISITES)
 	$(LIB_TOOL) $(LIB_CMD) $(LIB_OUT_FLAG)$@ $(OBJECT_FILES) $(CALLBACK_OBJECT_FILES)
+
+
+# PubNub SDK library with NTF runtime selection.
+# Note: Initialization vector make sense only if `OPENSSL` preprocessing flag 
+# 	 is set to `1`.
+pubnub_ntf_runtime_selection$(LIB_EXT): $(SOURCE_FILES) \
+		$(SYNC_SOURCE_FILES)     \
+		$(CALLBACK_SOURCE_FILES) \
+		$(NTF_RUNTIME_SELECTION_SOURCE_FILES)
+	$(COMPILER) \
+		$(OPTION_PREFIX)c                           \
+		$(COMPILER_FLAGS)                           \
+		$(CPPFLAGS)                                 \
+		$(CALLBACK_CPPFLAGS)                        \
+		$(NTF_SELECTION_CPPFLAGS)                   \
+		$(OPTION_PREFIX)U PUBNUB_RAND_INIT_VECTOR   \
+		$(OPTION_PREFIX)D PUBNUB_RAND_INIT_VECTOR=0 \
+		$(PREREQUISITES)
+
+# PubNub SDK library with NTF runtime selection and dynamic initialization 
+# vector.
+# Note: Initialization vector make sense only if `OPENSSL` preprocessing flag 
+# 	 is set to `1`.
+pubnub_ntf_runtime_selection_dynamiciv$(LIB_EXT): $(SOURCE_FILES) \
+		$(SYNC_SOURCE_FILES)     \
+		$(CALLBACK_SOURCE_FILES) \
+		$(NTF_RUNTIME_SELECTION_SOURCE_FILES)
+	$(COMPILER) \
+		$(OPTION_PREFIX)c                           \
+		$(COMPILER_FLAGS)                           \
+		$(CPPFLAGS)                                 \
+		$(CALLBACK_CPPFLAGS)                        \
+		$(NTF_SELECTION_CPPFLAGS)                   \
+		$(OPTION_PREFIX)U PUBNUB_RAND_INIT_VECTOR   \
+		$(OPTION_PREFIX)D PUBNUB_RAND_INIT_VECTOR=1 \
+		$(PREREQUISITES)
+
