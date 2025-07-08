20a983b94de092d2c9fc2f2f3f4bde9bb9a18bb2
Stephen Blum <stephen@pubnub.com>
2025-07-08 13:54:26 -0700

    xSemaphoreCreateRecursiveMutex (#220)
    * xSemaphoreCreateRecursiveMutex
    
    xSemaphoreCreateMutex to xSemaphoreCreateRecursiveMutex
    
    * Apply suggestions from code review
    
    * PubNub SDK v5.1.2 release.
    
    ---------
    
    Co-authored-by: PubNub Release Bot <120067856+pubnub-release-bot@users.noreply.github.com>

--- freertos/pbpal_mutex.h
+++ freertos/pbpal_mutex.h
@@ -10,18 +10,18 @@
 
 typedef SemaphoreHandle_t pbpal_mutex_t;
 
-#define pbpal_mutex_init(m) m = xSemaphoreCreateMutex();
-#define pbpal_mutex_lock(m) xSemaphoreTake(m, portMAX_DELAY)
-#define pbpal_mutex_unlock(m) xSemaphoreGive(m)
+#define pbpal_mutex_init(m) m = xSemaphoreCreateRecursiveMutex();
+#define pbpal_mutex_lock(m) xSemaphoreTakeRecursive(m, portMAX_DELAY)
+#define pbpal_mutex_unlock(m) xSemaphoreGiveRecursive(m)
 #define pbpal_mutex_destroy(m) vSemaphoreDelete(m)
-#define pbpal_mutex_decl_and_init(m) SemaphoreHandle_t m = xSemaphoreCreateMutex()
+#define pbpal_mutex_decl_and_init(m) SemaphoreHandle_t m = xSemaphoreCreateRecursiveMutex()
 #define pbpal_mutex_static_decl_and_init(m) static SemaphoreHandle_t m; static int m_init_##m
 
 // TODO: CRITICAL SECTION????
 #if ESP_PLATFORM
 #define pbpal_mutex_init_static(m) do { \
     if (0 == m_init_##m) { \
-        m = xSemaphoreCreateMutex(); \
+        m = xSemaphoreCreateRecursiveMutex(); \
         m_init_##m = 1; \
     } \
 } while(0)
@@ -29,7 +29,7 @@ typedef SemaphoreHandle_t pbpal_mutex_t;
 #define pbpal_mutex_init_static(m) do { \
     taskENTER_CRITICAL(m); \
     if (0 == m_init_##m) { \
-        m = xSemaphoreCreateMutex(); \
+        m = xSemaphoreCreateRecursiveMutex(); \
         m_init_##m = 1; \
     } \
     taskEXIT_CRITICAL(m); \
