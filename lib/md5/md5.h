afb5710ffc61e47401e633f654c99e186e5121fb
polar wu <polar.wu@ringcentral.com>
2025-07-17 13:37:27 +0800

    fix: compile conflict with openSSL

--- lib/md5/md5.h
+++ lib/md5/md5.h
@@ -36,8 +36,8 @@ typedef struct {
 	MD5_u32plus block[16];
 } MD5_CTX;
 
-extern void MD5_Init(MD5_CTX *ctx);
-extern void MD5_Update(MD5_CTX *ctx, void const *data, unsigned long size);
-extern void MD5_Final(unsigned char *result, MD5_CTX *ctx);
+extern void MD5_Init_(MD5_CTX *ctx);
+extern void MD5_Update_(MD5_CTX *ctx, void const *data, unsigned long size);
+extern void MD5_Final_(unsigned char *result, MD5_CTX *ctx);
 
 #endif
