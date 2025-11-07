a76c0c8a730933f2ce230e64c77c84bda2c29b7e
Serhii Mamontov <sergey@pubnub.com>
2025-11-07 15:24:25 +0200

    refactor(adapter-addresses): clean up from "dead" adapters
    Clean up `GetAdaptersAddresses` from the "dead" and unusable interfaces when searching for
    DNS server addresses.

--- windows/pubnub_dns_system_servers.c
+++ windows/pubnub_dns_system_servers.c
@@ -24,14 +24,14 @@ static bool copy_ipv4_bytes_from_be_dword(
     DWORD n_addr,
     unsigned char out[4])
 {
-    DWORD host_addr = ntohl(n_addr);
+    DWORD h_addr = ntohl(n_addr);
     unsigned char temp_ip[4];
     bool is_unique = false;
 
-    temp_ip[0] = (unsigned char)((host_addr >> 24) & 0xFF);
-    temp_ip[1] = (unsigned char)((host_addr >> 16) & 0xFF);
-    temp_ip[2] = (unsigned char)((host_addr >> 8) & 0xFF);
-    temp_ip[3] = (unsigned char)(host_addr & 0xFF);
+    temp_ip[0] = (unsigned char)((h_addr >> 24) & 0xFF);
+    temp_ip[1] = (unsigned char)((h_addr >> 16) & 0xFF);
+    temp_ip[2] = (unsigned char)((h_addr >> 8) & 0xFF);
+    temp_ip[3] = (unsigned char)(h_addr & 0xFF);
 
     for (size_t i = 0; i < count; i++) {
         if (memcmp(array[i].ipv4, temp_ip, 4) == 0) return false;
@@ -50,15 +50,9 @@ int fallback_get_dns_via_adapters(struct pubnub_ipv4_address* o_ipv4, size_t n)
     ULONG buflen;
     DWORD ret;
     IP_ADAPTER_ADDRESSES* addrs;
-    IP_ADAPTER_ADDRESSES* aa;
-    IP_ADAPTER_DNS_SERVER_ADDRESS* ds;
-    const struct sockaddr_in* sin;
     DWORD net_addr;
-    unsigned j;
+    unsigned j = 0;
     unsigned char temp_ip[4];
-
-    buflen = 0;
-    j = 0;
 
     /* Get required buffer size */
     ret = GetAdaptersAddresses(
@@ -69,13 +63,13 @@ int fallback_get_dns_via_adapters(struct pubnub_ipv4_address* o_ipv4, size_t n)
 
     if (ret != ERROR_BUFFER_OVERFLOW || buflen == 0) {
         PUBNUB_LOG_ERROR("GetAdaptersAddresses preflight failed: %lu\n", (unsigned long)ret);
-        return -1;
+        return (int)j;
     }
 
     addrs = (IP_ADAPTER_ADDRESSES*)MALLOC(buflen);
     if (!addrs) {
         PUBNUB_LOG_ERROR("OOM allocating %lu for GetAdaptersAddresses\n", (unsigned long)buflen);
-        return -1;
+        return (int)j;
     }
 
     /* Get adapter information */
@@ -87,18 +81,25 @@ int fallback_get_dns_via_adapters(struct pubnub_ipv4_address* o_ipv4, size_t n)
     if (ret != NO_ERROR) {
         PUBNUB_LOG_ERROR("GetAdaptersAddresses failed: %lu\n", (unsigned long)ret);
         FREE(addrs);
-        return -1;
+        return (int)j;
     }
 
     /* Enumerate adapters and collect unique DNS servers */
-    for (aa = addrs; aa && j < n; aa = aa->Next) {
-        for (ds = aa->FirstDnsServerAddress; ds && j < n; ds = ds->Next) {
+    for (IP_ADAPTER_ADDRESSES* aa = addrs; aa && j < n; aa = aa->Next) {
+        if (aa->OperStatus != IfOperStatusUp ||
+            aa->IfType == IF_TYPE_SOFTWARE_LOOPBACK ||
+            aa->IfType == IF_TYPE_TUNNEL ||
+            !(aa->Flags & IP_ADAPTER_IPV4_ENABLED)) {
+            continue;
+        }
+
+        for (IP_ADAPTER_DNS_SERVER_ADDRESS* ds = aa->FirstDnsServerAddress; ds && j < n; ds = ds->Next) {
             if (!ds->Address.lpSockaddr ||
                 ds->Address.lpSockaddr->sa_family != AF_INET) {
                 continue;
             }
 
-            sin = (const struct sockaddr_in*)ds->Address.lpSockaddr;
+            const struct sockaddr_in* sin = (const struct sockaddr_in*)ds->Address.lpSockaddr;
             net_addr = sin->sin_addr.S_un.S_addr;
             if (net_addr == 0) continue; /* skip 0.0.0.0 */
 
@@ -118,12 +119,7 @@ int pubnub_dns_read_system_servers_ipv4(struct pubnub_ipv4_address* o_ipv4, size
     DWORD buflen = 0;
     DNS_STATUS status = DnsQueryConfig(
         DnsConfigDnsServerList,
-        0,
-        NULL,
-        NULL,
-        NULL,
-        &buflen
-    );
+        0, NULL, NULL, NULL, &buflen);
 
     if (status == ERROR_SUCCESS && buflen >= sizeof(IP4_ARRAY)) {
         unsigned j = 0;
@@ -131,12 +127,7 @@ int pubnub_dns_read_system_servers_ipv4(struct pubnub_ipv4_address* o_ipv4, size
         if (ip4_list) {
             status = DnsQueryConfig(
                 DnsConfigDnsServerList,
-                0,
-                NULL,
-                NULL,
-                ip4_list,
-                &buflen
-            );
+                0, NULL, NULL, ip4_list, &buflen);
 
             if (status == ERROR_SUCCESS) {
                 for (DWORD i = 0; i < ip4_list->AddrCount && j < n; ++i) {
